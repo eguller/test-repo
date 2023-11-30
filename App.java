fb565f99be6cd25550e76e77f31e464a3918e55d
Engin Guller <enginguller@gmail.com>
2023-11-30 12:39:12 +0100

    add dummy java source

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public static String method1() {
+        return "method1";
+    }
+
+    public static String method2() {
+        return "method2";
+    }
+
+    public static String method3() {
+        return "method3";
+    }
+}
